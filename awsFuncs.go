9aa78e8c282b73d7c0e8bba11675fd9642cfc5d7
arran ubels <a.ubels@base2services.com>
2014-11-20 14:45:45 +1100

    Migrate functions minus tests across

---
+++ awsFuncs.go
@@ -0,0 +1,148 @@
+package awsdashboard
+
+import (
+		"net/url"
+		"net/http"
+		awsauth "github.com/smartystreets/go-aws-auth"
+		"io/ioutil"
+		"encoding/xml"
+		"strconv"
+)
+
+func getRegions(accessKey string, secretKey string, client *http.Client) (regionMap map[string]string, err error) {
+	regionMap = make(map[string]string)
+
+	var treq *http.Request
+	treq, err = http.NewRequest("GET", "https://ec2.ap-southeast-2.amazonaws.com/?Action=DescribeRegions&Version=2014-06-15", nil)
+	if err != nil  { return regionMap, err }
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	var tresp *http.Response
+	tresp, err = client.Do(treq)
+	if err != nil  { return regionMap, err }
+	var regions Regions = Regions{}
+	body,_ := ioutil.ReadAll(tresp.Body)
+	if err = xml.Unmarshal(body, &regions); err != nil  { return }
+
+	for _, each := range regions.RegionInfo {
+		regionMap[each.RegionName] = each.RegionEndpoint
+	}
+
+	return 
+}
+
+func getS3Object(accessKey string, secretKey string, bucketObjectUrl string, client *http.Client) (body []byte, err error) {
+	treq, err := http.NewRequest("GET", bucketObjectUrl, nil)
+	if err != nil { return }
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	tresp, err := client.Do(treq)
+	if err != nil  { return }
+
+	body,err = ioutil.ReadAll(tresp.Body)
+
+	return 
+}
+
+func getInstances(accessKey string, secretKey string, regionEndpoint string, client *http.Client, w http.ResponseWriter) (instanceList []Instance, err error) {
+	instanceList = []Instance{}
+	var treq *http.Request
+	treq, err = http.NewRequest("GET", "https://"+regionEndpoint+"/?Action=DescribeInstances&Version=2014-06-15", nil)
+	if err != nil { return }
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	var tresp *http.Response
+	tresp, err = client.Do(treq)
+	if err != nil  { return }
+	var instances Instances = Instances{}
+	body,_ := ioutil.ReadAll(tresp.Body)
+	if err = xml.Unmarshal(body, &instances); err != nil  { return }
+
+	for _, reservation := range instances.Reservations {
+		instanceList = append(instanceList, reservation.Instances...)
+	}
+
+	return 
+}
+
+func multiInstancesURL(regionEndpoint string, action string, instanceIds ...string) (ourl string, err error) {
+	params := url.Values{}
+	params,err = url.ParseQuery("Action=" + action + "&Version=2014-06-15")
+	if err != nil { return }
+	for index,id := range instanceIds {
+		params.Add("InstanceId." + strconv.Itoa(index),id)
+	}
+	ourl ="https://" + regionEndpoint + "/?" + params.Encode() 
+	return
+}
+
+func startInstances(accessKey string, secretKey string, regionEndpoint string, client *http.Client, _ http.ResponseWriter, instanceIds ...string) (instances StartInstance, err error, rurl string) {
+	var treq *http.Request
+	rurl, err = multiInstancesURL(regionEndpoint, "StartInstances", instanceIds...)
+	treq, err = http.NewRequest("GET", rurl, nil)
+	if err != nil { return }
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	var tresp *http.Response
+	tresp, err = client.Do(treq)
+	if err != nil  { return }
+	instances = StartInstance{}
+	body,_ := ioutil.ReadAll(tresp.Body)
+	if err = xml.Unmarshal(body, &instances); err != nil  { return }
+
+	return 
+}
+
+func stopInstances(accessKey string, secretKey string, regionEndpoint string, client *http.Client, _ http.ResponseWriter, instanceIds ...string) (instances StartInstance, err error, rurl string) {
+	var treq *http.Request
+	rurl, err = multiInstancesURL(regionEndpoint, "StopInstances", instanceIds...)
+	treq, err = http.NewRequest("GET", rurl, nil)
+	if err != nil { return }
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	var tresp *http.Response
+	tresp, err = client.Do(treq)
+	if err != nil  { return }
+	instances = StartInstance{}
+	body,_ := ioutil.ReadAll(tresp.Body)
+	if err = xml.Unmarshal(body, &instances); err != nil  { return }
+
+	return 
+}
+
+func getInstancesStatus(accessKey string, secretKey string, regionEndpoint string, client *http.Client, w http.ResponseWriter, all bool, instanceIds ...string) (instantStatuses InstantStatuses, err error) {
+	var treq *http.Request
+	rurl, err := multiInstancesURL(regionEndpoint, "DescribeInstanceStatus", instanceIds...)
+	if all {
+		rurl = rurl+"&IncludeAllInstances=true"
+	}
+	treq, err = http.NewRequest("GET", rurl, nil)
+	awscred := awsauth.Credentials{
+		AccessKeyID: accessKey,
+		SecretAccessKey: secretKey,
+	}
+	awsauth.Sign(treq, awscred)
+	var tresp *http.Response
+	tresp, err = client.Do(treq)
+	if err != nil  { return }
+	instantStatuses = InstantStatuses{}
+	body,_ := ioutil.ReadAll(tresp.Body)
+	if err = xml.Unmarshal(body, &instantStatuses); err != nil  { return }
+	return
+}
+
---
+++ awsStructs.go
@@ -0,0 +1,63 @@
+package awsdashboard
+
+type RegionInfo struct {
+	RegionName string `xml:"regionName"`
+	RegionEndpoint string `xml:"regionEndpoint"`
+}
+
+type AwsRequest struct {
+	RequestId string `xml:"requestId"`
+}
+
+type Regions struct {
+	AwsRequest
+	RegionInfo []RegionInfo `xml:"regionInfo>item"`
+}
+
+type Tag struct {
+	Key string `xml:"key"`
+	Value string `xml:"value"`
+}
+
+type Instance struct { // just add what ever you need here when you need it.
+	InstanceId string `xml:"instanceId",json:"Id"`
+	Architecture string `xml:"architecture"`
+	InstanceType string `xml:"instanceType"`
+	PrivateDnsName string `xml:"privateDnsName"`
+	Tags []Tag `xml:"tagSet>item"`
+	IpAddress string `xml:"ipAddress"`
+	InstanceState string `xml:"instanceState>name"`
+	CurrentInstanceState string `xml:"currentState>name"`
+	PreviousInstanceState string `xml:"previousState>name"`
+	InstanceStateCode string `xml:"instanceState>code"`
+	ProfileName string // custom field
+	Region string // custom field
+}
+
+type Group struct {
+	GroupId string `xml:"groupId"`
+	GroupName string `xml:"groupName"`
+}
+
+type Reservation struct {
+	ReservationId string `xml:"reservationId"`
+	OwnerId string `xml:"ownerId"`
+	Groups []Group `xml:"groupSet>item"`
+	Instances []Instance `xml:"instancesSet>item"`
+}
+
+type InstantStatuses struct {
+	AwsRequest
+	Instances []Instance `xml:"instanceStatusSet>item"`
+}
+
+type Instances struct {
+	AwsRequest
+	Reservations []Reservation `xml:"reservationSet>item"`
+}
+
+type StartInstance struct {
+	AwsRequest
+	Instances []Instance `xml:"instancesSet>item"`
+}
+
